0b0dacb5a402c49b9b023aaf50779682fcbb187a
Karol <karol.jurczenia@gmail.com>
2016-03-13 20:30:43 +0100

    Added files via upload

--- BSK/Form1.cs
+++ BSK/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace BSK
 {
-    public partial class Form1 : Form
+    public partial class Szyfrator : Form
     {
-        public Form1()
+        public Szyfrator()
         {
             InitializeComponent();
         }
